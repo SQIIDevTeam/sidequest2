7580b4bc0c921053ec5f9aa9baccaca2897a8869
Patrick Hahn <patricksilashahn@gmx.net>
2013-12-29 21:56:06 +0100

    Added Libraries + initial main code

---
+++ SideQuest II/SideQuest II/Libs.cpp
@@ -0,0 +1,15 @@
+#ifdef _DEBUG
+#pragma comment(lib, "sfml-audio-d.lib")
+#pragma comment(lib, "sfml-graphics-d.lib")
+#pragma comment(lib, "sfml-main-d.lib")
+#pragma comment(lib, "sfml-network-d.lib")
+#pragma comment(lib, "sfml-system-d.lib")
+#pragma comment(lib, "sfml-window-d.lib")
+#elif
+#pragma comment(lib, "sfml-audio.lib")
+#pragma comment(lib, "sfml-graphics.lib")
+#pragma comment(lib, "sfml-main.lib")
+#pragma comment(lib, "sfml-network.lib")
+#pragma comment(lib, "sfml-system.lib")
+#pragma comment(lib, "sfml-window.lib")
+#endif
---
+++ SideQuest II/SideQuest II/SideQuestMain.cpp
@@ -0,0 +1,8 @@
+#include <cstdlib>
+#include <iostream>
+int main()
+{
+	using std::cout;
+	cout << "Starting up SideQuest II\n";
+	return EXIT_SUCCESS;
+}
