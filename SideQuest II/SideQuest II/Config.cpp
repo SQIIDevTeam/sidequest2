6fe2143a3d96e3d0341ece92dbc01ccb4025c473
Markus Hackspacher <hackspacher@gmx.de>
2014-02-02 14:59:08 +0100

    add fault message to config.cpp

--- SideQuest II/SideQuest II/Config.cpp
+++ SideQuest II/SideQuest II/Config.cpp
@@ -1,11 +1,17 @@
 #include "Config.h"
 #include <fstream>
 #include <sstream>
-
+#include <iostream>
 
 Config::Config(std::string filename)
 /** read the config file */{
 	std::ifstream file(filename, std::ios::in);
+	if (file == 0)
+	//Exit if no file found  
+	{
+		std::cout << "No file found: " << filename << "\n"; 
+		return;
+	}
 	while (!file.eof())
 	{
 		std::string line;
