f38e2f774443a55e6fbdd9f06827f416926249d6
Markus Hackspacher <hackspacher@gmx.de>
2014-02-02 15:12:56 +0100

    change font name

--- SideQuest II/SideQuest II/MainMenuState.cpp
+++ SideQuest II/SideQuest II/MainMenuState.cpp
@@ -9,16 +9,16 @@ MainMenuState::MainMenuState(App& app)
 	m_menu.setOrigin(m_menu.getSize().x / 2.f, m_menu.getSize().y / 2.f);
 	m_menu.setPosition(m_app.window.getView().getCenter());
 
-	m_menu.addButton("Assets/Menu/button.png", "Assets/Menu/button_hover.png", "Assets/Fonts/ALGER.ttf", "Play", [&](sf::Vector2f){
+	m_menu.addButton("Assets/Menu/button.png", "Assets/Menu/button_hover.png", "Assets/Fonts/ALGER.TTF", "Play", [&](sf::Vector2f){
 		m_app.statemanager.pushState("play");
 	});
-	m_menu.addButton("Assets/Menu/button.png", "Assets/Menu/button_hover.png", "Assets/Fonts/ALGER.ttf", "Options", [&](sf::Vector2f){
+	m_menu.addButton("Assets/Menu/button.png", "Assets/Menu/button_hover.png", "Assets/Fonts/ALGER.TTF", "Options", [&](sf::Vector2f){
 		m_app.statemanager.pushState("options");
 	});
-	m_menu.addButton("Assets/Menu/button.png", "Assets/Menu/button_hover.png", "Assets/Fonts/ALGER.ttf", "Credits", [&](sf::Vector2f){
+	m_menu.addButton("Assets/Menu/button.png", "Assets/Menu/button_hover.png", "Assets/Fonts/ALGER.TTF", "Credits", [&](sf::Vector2f){
 		m_app.statemanager.pushState("credits");
 	});
-	m_menu.addButton("Assets/Menu/button.png", "Assets/Menu/button_hover.png", "Assets/Fonts/ALGER.ttf", "Exit", [&](sf::Vector2f){
+	m_menu.addButton("Assets/Menu/button.png", "Assets/Menu/button_hover.png", "Assets/Fonts/ALGER.TTF", "Exit", [&](sf::Vector2f){
 		m_app.statemanager.setRunning(false);
 	});
 }
