0e341166f93d283f58e3c5d99335fc9e22f4f141
Markus Hackspacher <hackspacher@gmx.de>
2014-02-03 19:41:57 +0100

    logo in documentation

--- SideQuest II/SideQuest II/Config.cpp
+++ SideQuest II/SideQuest II/Config.cpp
@@ -3,25 +3,14 @@
 #include <sstream>
 #include <iostream>
 
-<<<<<<< HEAD
-=======
 /** read the config file */
->>>>>>> c6ea8a111ee6b09ee0d63ea0a27a942949897cef
 Config::Config(std::string filename)
-/** read the config file */{
+{
 	std::ifstream file(filename, std::ios::in);
-<<<<<<< HEAD
-	if (file == 0)
-	//Exit if no file found  
-	{
-		std::cout << "No file found: " << filename << "\n"; 
-		return;
-=======
 	if (!file)
 	{
 		std::cerr << "Could not load config file: " << filename << '\n';
 		throw std::runtime_error(std::string("Config::(constructor): Could not load config file ") + filename);
->>>>>>> c6ea8a111ee6b09ee0d63ea0a27a942949897cef
 	}
 	while (!file.eof())
 	{
